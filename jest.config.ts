00e0f03fc764459b5bd06fb0bd126e6de51998e7
zehan12 <zehan9211@gmail.com>
2025-02-12 13:55:00 +0530

    test: :test_tube: add jest config

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+import { Config } from "jest";
+
+const config: Config = {
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.{ts,tsx}",
+        "!src/**/*.d.ts",
+        "!**/vendor/**",
+    ],
+    coverageDirectory: "coverage",
+    testEnvironment: "jsdom",
+    transform: {
+        ".(ts|tsx)": ["ts-jest", { tsconfig: "tsconfig.app.json" }],
+    },
+    coverageReporters: ["json-summary", "clover", "text"],
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/coverage",
+        "package.json",
+        "package-lock.json",
+        "pnpm-lock.json",
+        "yarn-lock.json",
+        "jest.setup.ts",
+    ],
+    moduleNameMapper: {
+        "\\.(css|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.ts",
+    },
+    setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+};
+
+module.exports = config;

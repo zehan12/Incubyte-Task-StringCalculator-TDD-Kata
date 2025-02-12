e45b4fc4b986cf98b0cf95654378bc84b543179e
zehan12 <zehan9211@gmail.com>
2025-02-12 20:55:00 +0530

    style: :art: integrate tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   };
   return (
     <main role="main">
+      <h1 className="text-center text-4xl font-bold text-zinc-800/90">String Calculator</h1>
       <Button label="Click" onClick={handleClick} />
     </main>
   );

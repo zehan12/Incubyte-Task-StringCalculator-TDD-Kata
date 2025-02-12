ff1caa65d87da268bb46a2fc6299217baf4fbe7e
zehan12 <zehan9211@gmail.com>
2025-02-12 15:23:00 +0530

    refactor: :recycle: refactor code

--- src/core/StringCalculator.ts
+++ src/core/StringCalculator.ts
@@ -1,24 +1,13 @@
 export class StringCalculator {
     static Add(numbers: string): number {
-        // Handle empty string
-        if (numbers.trim() === "") {
+        if (!numbers.trim()) {
             return 0;
         }
 
-        // Handle single string number
-        if (numbers.length === 1 && !isNaN(+numbers)) {
-            return +numbers;
-        }
-
-        // Handle comma separated string number
-        if (numbers.length > 1 && numbers.includes(",") ) {
-            const splitNumbers = numbers.split(",");
-            const sum = splitNumbers
-                .map(Number)
-                .reduce((acc, cv) => (acc += cv), 0);
-            return sum;
-        }
-
-        return 0;
+        const parsedNumbers = numbers
+            .split(",")
+            .map(Number)
+            .filter((n) => !isNaN(n));
+        return parsedNumbers.reduce((sum, num) => sum + num, 0);
     }
 }
--- src/tests/core/stringCalculator.spec.ts
+++ src/tests/core/stringCalculator.spec.ts
@@ -23,4 +23,6 @@ describe("String Calculator", () => {
 
     });
 
+    
+
 });

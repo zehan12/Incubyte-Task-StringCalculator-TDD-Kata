ea6b7606111928b6bc5b09bf2af96855aa6bc065
zehan12 <zehan9211@gmail.com>
2025-02-13 10:00:00 +0530

    fix: :ambulance: fix jest issue

--- jest.config.ts
+++ jest.config.ts
@@ -2,15 +2,19 @@ import { Config } from "jest";
 
 const config: Config = {
     collectCoverage: true,
-    // collectCoverageFrom: [
-    //     "src/**/*.{ts,tsx}",
-    //     "!src/**/*.d.ts",
-    //     "!**/vendor/**",
-    // ],
+    collectCoverageFrom: [
+        "src/**/*.{ts,tsx}",
+        "!src/**/hooks/*.{ts,tsx}",
+        "!src/**/lib/*.{ts,tsx}",
+        "!src/**/components/ui/*.{ts,tsx}",
+        "!src/**/*.d.ts",
+        "!**/vendor/**",
+    ],
     coverageDirectory: "coverage",
     testEnvironment: "jsdom",
     transform: {
         ".(ts|tsx)": ["ts-jest", { tsconfig: "tsconfig.app.json" }],
+        "^.+\\.svg$": "jest-transformer-svg",
     },
     coverageReporters: ["json-summary", "clover", "text"],
     coveragePathIgnorePatterns: [
@@ -24,6 +28,7 @@ const config: Config = {
     ],
     moduleNameMapper: {
         "\\.(css|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.ts",
+        "^@/(.*)$": "<rootDir>/src/$1",
     },
     setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
 };
